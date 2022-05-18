4f8c8e2f25e566d1de660315c4448b2428f55ac6
take-cantik <take.cantik17@gmail.com>
2022-05-18 16:19:47 +0900

    Add: liff hook

---
+++ hook/liff.ts
@@ -0,0 +1,32 @@
+import type { Liff } from "@line/liff";
+import { useEffect, useState } from "react";
+
+export const useLiff = () => {
+  const [liffObject, setLiffObject] = useState<Liff | null>(null);
+  const [liffError, setLiffError] = useState<string | null>(null);
+
+  // Execute liff.init() when the app is initialized
+  useEffect(() => {
+    // to avoid `window is not defined` error
+    import("@line/liff")
+      .then((liff) => liff.default)
+      .then((liff) => {
+        console.log("LIFF init...");
+        liff
+          .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID || "" })
+          .then(() => {
+            console.log("LIFF init succeeded.");
+            setLiffObject(liff);
+          })
+          .catch((error: Error) => {
+            console.log("LIFF init failed.");
+            setLiffError(error.toString());
+          });
+      });
+  }, []);
+
+  return {
+    liffObject,
+    liffError
+  };
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,35 @@
 import "../../styles/globals.css";
 import "../../styles/reset.css";
 import type { AppProps } from "next/app";
-import type { Liff } from "@line/liff";
-import { useState, useEffect } from "react";
+// import type { Liff } from "@line/liff";
+// import { useState, useEffect } from "react";
+import { useLiff } from "hook/liff";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const [liffObject, setLiffObject] = useState<Liff | null>(null);
-  const [liffError, setLiffError] = useState<string | null>(null);
+  // const [liffObject, setLiffObject] = useState<Liff | null>(null);
+  // const [liffError, setLiffError] = useState<string | null>(null);
 
-  // Execute liff.init() when the app is initialized
-  useEffect(() => {
-    // to avoid `window is not defined` error
-    import("@line/liff")
-      .then((liff) => liff.default)
-      .then((liff) => {
-        console.log("LIFF init...");
-        liff
-          .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID || "" })
-          .then(() => {
-            console.log("LIFF init succeeded.");
-            setLiffObject(liff);
-          })
-          .catch((error: Error) => {
-            console.log("LIFF init failed.");
-            setLiffError(error.toString());
-          });
-      });
-  }, []);
+  // // Execute liff.init() when the app is initialized
+  // useEffect(() => {
+  //   // to avoid `window is not defined` error
+  //   import("@line/liff")
+  //     .then((liff) => liff.default)
+  //     .then((liff) => {
+  //       console.log("LIFF init...");
+  //       liff
+  //         .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID || "" })
+  //         .then(() => {
+  //           console.log("LIFF init succeeded.");
+  //           setLiffObject(liff);
+  //         })
+  //         .catch((error: Error) => {
+  //           console.log("LIFF init failed.");
+  //           setLiffError(error.toString());
+  //         });
+  //     });
+  // }, []);
+
+  const { liffObject, liffError } = useLiff();
 
   // Provide `liff` object and `liffError` object
   // to page component as property
